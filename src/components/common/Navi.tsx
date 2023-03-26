10c5a479880342c13e41174539821f175665230b
dohyeon.jeon <dohyeon.jeon@navercorp.com>
2023-03-26 20:30:33 +0900

    Navi bar

--- src/components/common/Navi.tsx
+++ src/components/common/Navi.tsx
@@ -1,18 +1,18 @@
-import { Drawer, IconButton, List, ListItem, ListItemIcon, ListItemText } from '@material-ui/core';
-import CloseIcon from '@material-ui/icons/Close';
+import { Drawer, IconButton } from '@material-ui/core';
+import { ContactSupportOutlined, SettingsOutlined } from '@material-ui/icons';
 import MenuIcon from '@material-ui/icons/Menu';
 import Search from '@material-ui/icons/Search';
-import Link from 'next/link';
 import React, { useState } from 'react';
 import styled from 'styled-components';
 
-type LinkItem = {
-  href: string;
-  label: string;
+type User = {
+  nickname: string;
+  src: string;
+  alt: string;
 };
 
 type NavbarProps = {
-  links: LinkItem[];
+  user: User | undefined;
 };
 
 const NavbarWrapper = styled.div`
@@ -52,49 +52,62 @@ const NavbarSearchButton = styled(IconButton)`
 
 const NavbarDrawer = styled(Drawer)`
   && .MuiDrawer-paper {
-    width: 240px;
-    background-color: #333;
-    color: #fff;
+    width: 60%;
+    background-color: ##efefef;
+    border-top-right-radius: 10px;
+    border-bottom-right-radius: 10px;
+    display: flex;
+    justify-content: space-between;
   }
 `;
 
-const NavbarCloseButton = styled(IconButton)`
-  && {
-    color: #fff;
-    margin-right: 8px;
-    align-self: flex-end;
-  }
-
-  &&:hover {
-    background-color: rgba(255, 255, 255, 0.1);
-  }
+const MenuContainer = styled.div`
+  margin: 48px 24px;
 `;
 
-const NavbarLinks = styled(List)`
-  && .MuiListItem-root {
-    color: #fff;
-  }
+const WelcomeText = styled.div`
+  font-weight: 600;
+  font-size: 10px;
+  line-height: 12px;
 `;
 
-const NavbarLinkItem = styled(ListItem)`
-  &&:hover {
-    background-color: rgba(255, 255, 255, 0.1);
-  }
+const UserContainer = styled.div`
+  margin: 16px 0 80px 0;
 `;
 
-const NavbarLinkIcon = styled(ListItemIcon)`
-  && {
-    color: #fff;
-  }
+const LoginText = styled.div`
+  font-weight: 700;
+  font-size: 14px;
+  line-height: 17px;
 `;
 
-const NavbarLinkText = styled(ListItemText)`
-  && {
-    font-size: 1rem;
-  }
+const NavLinkWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 16px;
 `;
 
-const Navbar: React.FC<NavbarProps> = ({ links }) => {
+const NavLinkText = styled.div`
+  font-weight: 700;
+  font-size: 14px;
+  line-height: 17px;
+  margin-left: 8px;
+`;
+
+const VersionContainer = styled.div`
+  margin: 48px 24px;
+`;
+
+const VersionText = styled.div`
+  font-weight: 600;
+  font-size: 10px;
+  line-height: 12px;
+  margin-top: 8px;
+
+  color: #52525c;
+`;
+
+const Navbar: React.FC<NavbarProps> = ({ user }) => {
   const [isDrawerOpen, setIsDrawerOpen] = useState(false);
 
   const handleDrawerOpen = () => {
@@ -114,20 +127,26 @@ const Navbar: React.FC<NavbarProps> = ({ links }) => {
       <NavbarSearchButton>
         <Search />
       </NavbarSearchButton>
+
       <NavbarDrawer anchor="left" open={isDrawerOpen} onClose={handleDrawerClose}>
-        <NavbarCloseButton onClick={handleDrawerClose}>
-          <CloseIcon />
-        </NavbarCloseButton>
-        <NavbarLinks>
-          {links.map((link) => (
-            <Link key={link.href} href={link.href} passHref>
-              <NavbarLinkItem button onClick={handleDrawerClose}>
-                <NavbarLinkIcon>{/* Icon */}</NavbarLinkIcon>
-                <NavbarLinkText primary={link.label} />
-              </NavbarLinkItem>
-            </Link>
-          ))}
-        </NavbarLinks>
+        <MenuContainer>
+          <WelcomeText>반가워요!</WelcomeText>
+          <UserContainer>
+            <LoginText>{'로그인 해주세요 >'}</LoginText>
+          </UserContainer>
+          <NavLinkWrapper>
+            <SettingsOutlined style={{ width: 14, height: 14 }} />
+            <NavLinkText>설정</NavLinkText>
+          </NavLinkWrapper>
+          <NavLinkWrapper>
+            <ContactSupportOutlined style={{ width: 14, height: 14 }} />
+            <NavLinkText>문의하기</NavLinkText>
+          </NavLinkWrapper>
+        </MenuContainer>
+        <VersionContainer>
+          <VersionText>어플리케이션 버전 v 1.00</VersionText>
+          <VersionText>팀명</VersionText>
+        </VersionContainer>
       </NavbarDrawer>
     </NavbarWrapper>
   );
