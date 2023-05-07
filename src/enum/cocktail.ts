a4927fa699c58a3e79d89ab39432e81195f3703d
정은주 <lynn0506@snu.ac.kr>
2023-05-08 01:45:06 +0900

    [Cocktail-Recipe/issues#39] state, enum, const, url 등 정의 및 수정

--- src/enum/cocktail.ts
+++ src/enum/cocktail.ts
@@ -1,12 +1,16 @@
 export enum CocktailBaseAlcohol {
   RUM = 'RUM',
   VODKA = 'VODKA',
-  WHISKEY = 'WHISKEY', // static image 이름 오타 whisky로 되어 있음
+  WHISKEY = 'WHISKEY',
   GIN = 'GIN',
   TEQUILA = 'TEQUILA',
   SOJU = 'SOJU',
   BRANDY = 'BRANDY',
-  NON_ALCOHOL = 'NON-ALCOHOL',
+
+  // NON_ALCOHOLIC 이랑 혼용되고 있음 -> 명칭 하나로 통일해 줄 수 있는지 요청
+  // 1. 이미지 파일은 NON-ALCOHOL.png
+  // 2. 서버 칵테일 데이터는 { baseAlcohol: NON_ALCOHOLIC }
+  NON_ALCOHOL = 'NON_ALCOHOLIC',
 }
 
 export enum CocktailStatus {
