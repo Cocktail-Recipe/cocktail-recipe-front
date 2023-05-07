3802996e77d961b18ce8706c296dd6f93c6e5c74
정은주 <lynn0506@snu.ac.kr>
2023-05-08 01:37:29 +0900

    [Cocktail-Recipe/issues#39] 변경된 서버 url 적용

--- src/api/cocktail.ts
+++ src/api/cocktail.ts
@@ -1,10 +1,10 @@
 import { CocktailSearchRequestState } from '@/states/cocktail/cocktailSearchRequest.state';
-import { axiosInstance } from './axios';
 import { CocktailListResponse } from '@/models/cocktail.model';
+import { axiosInstance } from './axios';
 
 const cocktailAPI = {
   getAllCocktails: (params: CocktailSearchRequestState) => {
-    return axiosInstance.get<CocktailListResponse>('/app/cocktails', { params });
+    return axiosInstance.get<CocktailListResponse>('/app/cocktails/', { params });
   },
 };
 
