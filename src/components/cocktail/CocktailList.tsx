0c1d0f9e92c67fa33445162454d252fa8e8612a5
정은주 <lynn0506@snu.ac.kr>
2023-05-08 11:06:24 +0900

    [Cocktail-Recipe/issues#40] 칵테일 리스트 무한 스크롤 구현

--- src/components/cocktail/CocktailList.tsx
+++ src/components/cocktail/CocktailList.tsx
@@ -1,19 +1,58 @@
-import { List } from 'antd';
-import React, { ReactElement } from 'react';
-import { Cocktail } from '@/models/cocktail.model';
+import React, { ReactElement, useCallback, useMemo } from 'react';
+import { FixedSizeList as List } from 'react-window';
+import { useRecoilValue } from 'recoil';
+import InfiniteLoader from 'react-window-infinite-loader';
+import AutoSizer from 'react-virtualized-auto-sizer';
+
+import useCocktailList from '@/hook/useCocktailList';
+import { cocktailListState } from '@/states/cocktail/cocktailList.state';
 import CocktailCard from './CocktailCard';
+import { Spin } from 'antd';
 
-interface CockTailListProps {
-  cocktails: Cocktail[];
-}
+const CockTailList = (): ReactElement => {
+  const cocktails = useRecoilValue(cocktailListState);
+  const { loadMoreCocktails, hasNextPage, isLoading } = useCocktailList();
 
-const CockTailList = ({ cocktails }: CockTailListProps): ReactElement => {
+  const CocktailItem = useCallback(
+    ({ index, style }: any) => (
+      <CocktailCard cocktail={cocktails[index]} key={cocktails[index]?.cocktailId} style={style} />
+    ),
+    [cocktails],
+  );
+
+  const itemCount = useMemo(
+    () => (hasNextPage ? cocktails.length + 1 : cocktails.length),
+    [cocktails.length, hasNextPage],
+  );
+
+  const isItemLoaded = useCallback(
+    (index: number) => !hasNextPage || index < cocktails.length,
+    [cocktails.length, hasNextPage],
+  );
+
   return (
-    <List
-      itemLayout="horizontal"
-      dataSource={cocktails}
-      renderItem={(cocktail: Cocktail) => <CocktailCard cocktail={cocktail} key={cocktail.cockTailId} />}
-    />
+    <AutoSizer>
+      {({ height, width }) => (
+        <InfiniteLoader isItemLoaded={isItemLoaded} itemCount={itemCount} loadMoreItems={loadMoreCocktails}>
+          {({ onItemsRendered, ref }) =>
+            isLoading ? (
+              <Spin />
+            ) : (
+              <List
+                height={height as number}
+                width={width as number}
+                itemSize={170}
+                onItemsRendered={onItemsRendered}
+                ref={ref}
+                itemCount={itemCount}
+              >
+                {CocktailItem}
+              </List>
+            )
+          }
+        </InfiniteLoader>
+      )}
+    </AutoSizer>
   );
 };
 
