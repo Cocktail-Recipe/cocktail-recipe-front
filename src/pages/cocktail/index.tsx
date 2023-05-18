4796b9b8d0d4a5a4d7f58fbb4b2a01290efdd0d4
정은주 <lynn0506@snu.ac.kr>
2023-05-19 01:38:54 +0900

    [Cocktail-Recipe/issues#43] 백링크 header 컴포 구현 및 props로 header를 반도록 수정, 페이지 컴포 이름 변경

--- src/pages/cocktail/index.tsx
+++ src/pages/cocktail/index.tsx
@@ -5,12 +5,13 @@ import AppLayout from '@/components/layout/AppLayout';
 import CocktailSearch from '@/components/cocktail-filter/CocktailSearchInput';
 import CockTailList from '@/components/cocktail-list/CocktailList';
 import CocktailSearchFilterContainer from '@/components/cocktail-filter/CocktailSearchFilterContainer';
+import Header from '@/components/layout/header/Header';
 
 const { Text } = Typography;
 
-const Cocktail = (): ReactElement => {
+const CocktailPage = (): ReactElement => {
   return (
-    <AppLayout>
+    <AppLayout header={<Header />}>
       <>
         <Text>칵테일</Text>
         <CocktailSearch />
@@ -21,4 +22,4 @@ const Cocktail = (): ReactElement => {
   );
 };
 
-export default Cocktail;
+export default CocktailPage;
--- src/pages/myPage/index.tsx
+++ src/pages/myPage/index.tsx
@@ -1,9 +1,10 @@
 import { ReactElement } from 'react';
 import AppLayout from '@/components/layout/AppLayout';
+import Header from '@/components/layout/header/Header';
 
 const MyPage = (): ReactElement => {
   return (
-    <AppLayout>
+    <AppLayout header={<Header />}>
       <>마이페이지</>
     </AppLayout>
   );
